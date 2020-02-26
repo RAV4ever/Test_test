ecb0597c6f1374270ce94442921bdcb40d8d343e
unknown <kyriacosthe1st@gmail.com>
2020-02-26 16:38:02 +0000

    First Comment

---
+++ classes.py
@@ -0,0 +1,22 @@
+class Employee:
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + "@company.com"
+        # FirstNAME
+
+
+    def fullname(self):
+        return self.first + " " + self.last + " is learning to program in Python."
+
+    def DeansList(self):
+        return "Yousef Nami"
+
+class Test:
+
+    def DeansList():
+        return "Yousef Nami"
+
+    def DeansList(self):
+        return "Yousef Nami"
